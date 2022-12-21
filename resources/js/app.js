7c6077119b6bc6c0a42300ef933df85363b6b5e2
Andrey Grytsenko <andreyapg@gmail.com>
2022-12-21 17:20:34 +0200

    Add Vue (has to be fixed 2022-12-21

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import Vue from "vue";
+
+window.Vue = Vue;
+
+import User from "./components/User.vue";
+Vue.component('User', User);
+
+new Vue({ el: '#app', render: h => h(User) })
---
+++ resources/js/components/User.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <h2>This is User.vue</h2>
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        You are logged in! From
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "User"
+}
+</script>
